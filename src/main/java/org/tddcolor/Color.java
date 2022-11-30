c136a9f810ca198db66fc4536ebe09223f3d0719
Shaza Aldawamneh <118107805@umail.ucc.ie>
2022-11-30 19:16:12 +0000

    first commit

---
+++ src/main/java/org/tddcolor/Color.java
@@ -0,0 +1,14 @@
+package org.tddcolor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
